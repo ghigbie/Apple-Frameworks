a9fff4f8007c84d103a6fb988df04f2cb12a8fbf
ghigbie <gehigbie@gmail.com>
2021-01-24 19:27:46 -0500

    Add to detail page

---
+++ Apple-Frameworks/views/CloseButton.swift
@@ -0,0 +1,20 @@
+//
+//  CloseButton.swift
+//  Apple-Frameworks
+//
+//  Created by George Higbie on 1/24/21.
+//
+
+import SwiftUI
+
+struct CloseButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CloseButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CloseButton()
+    }
+}
--- Apple-Frameworks/views/DetailView.swift
+++ Apple-Frameworks/views/DetailView.swift
@@ -8,19 +8,24 @@
 import SwiftUI
 
 struct DetailView: View {
+    @Environment(\.colorScheme) var colorScheme
     var framework: Framework = MockData.sampleFramework
     
     var body: some View {
         VStack{
             HStack{
                 Spacer()
-                
+                Button(){
+                    print("Close \(framework.name)")
+                }label: {
+                    Image(systemName:"xmark")
+                }
+                .foregroundColor(colorScheme == .dark ? .white : .black)
             }
             Spacer()
             FrameworkTitleView(framework: framework)
             Spacer()
             Text(framework.description)
-                .padding()
             Spacer()
             Button{
                 print("Do someting \(framework.name)")
@@ -31,13 +36,13 @@ struct DetailView: View {
                     backgroundColor: .red
                 )
             }
-        }
+        }.padding()
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView()
-            .
+           // .preferredColorScheme(.dark)
     }
 }
